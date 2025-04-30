4ecdeff0aa04f3eeeca3a5d38dfd7fdcb37daccb
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-04-30 15:47:49 -0500

    Added CSV transaction logging

--- FlexFund/src/main/java/com/pluralsight/TransactionManager.java
+++ FlexFund/src/main/java/com/pluralsight/TransactionManager.java
@@ -1,23 +1,42 @@
 package com.pluralsight;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class TransactionManager {
     private List <Transaction> transactions = new ArrayList<>();
+    private final String File_Name = "transactions.csv";
 
+    public TransactionManager() {
+        loadTransactionsFromFile(); // Loads transaction from start
+    }
 
-    public void addTransaction(String vendor, double amount){
+    public void addTransaction(Scanner scanner, String type) {
+        System.out.println("Enter vendor");
+        String vendor = scanner.next();
+
+        System.out.println("Enter amount: ");
+        double amount = Double.parseDouble(scanner.nextLine());
+
+        if (type.equals("payment")) {
+            amount = -Math.abs(amount); // payments are stored negative diff. from deposit
+        }
+
         Transaction transaction = new Transaction(vendor, amount);
         transactions.add(transaction);
-        System.out.println(transaction.getType() + " recorded for " + vendor + ": $" + Math.abs(amount));
+        writeTransactionToFile(transactions); // save to CSV
+        System.out.println("Transaction added.");
+
     }
 
     public void listTransactions() {
         if (transactions.isEmpty()) {
             System.out.println("No transactions yet.");
-        } else {
-            for (Transaction t : transactions) {
-                System.out.println(t.getType() + " | Vendor: " + t.getVendor() + " | Amount: $" + Math.abs(t.getAmount()));
+            return;
+        }
+        System.out.println("=== Recent Transactions (Newest First) ===");
+            for (int i = transactions.size() -1; i >= 0; i--) {
+                System.out.println(transactions.get(i));
             }
         }
     }
